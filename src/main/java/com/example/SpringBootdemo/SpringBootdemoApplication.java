ddd89e341ec24c62982a5deb0dc4f3f577787883
harirebel455 <79778945+harirebel455@users.noreply.github.com>
2024-11-03 11:25:29 +0530

    Ram

--- src/main/java/com/example/SpringBootdemo/SpringBootdemoApplication.java
+++ src/main/java/com/example/SpringBootdemo/SpringBootdemoApplication.java
@@ -8,6 +8,8 @@ public class SpringBootdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootdemoApplication.class, args);
+
+		System.out.println("Hi ram");
 	}
 
 }
